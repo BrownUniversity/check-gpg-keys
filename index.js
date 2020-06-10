566a87525b69d99eee45ede32490d3b1214f8759
Sumner Warren <sumner_warren@brown.edu>
2020-06-09 22:07:39 -0400

    fetch github data

--- index.js
+++ index.js
@@ -1,9 +1,66 @@
 const core = require("@actions/core");
 const github = require("@actions/github");
 
+function getKeys(keyringDir) {
+  return Promise.resolve([
+    {
+      status: "valid",
+      email: "sumner_warren@brown.edu"
+    },
+    {
+      status: "expiring",
+      email: "sumner_warren@brown.edu"
+    },
+    {
+      status: "expired",
+      email: "sumner_warren@brown.edu"
+    }
+  ]);
+}
+
+async function getRepoData(octokit, repo) {
+  const data = await octokit.graphql(
+    `
+      {
+        repository(owner: "BrownUniversity", name: $repo) {
+          assignableUsers(first: 50) {
+            edges {
+              node {
+                login
+                email
+              }
+            }
+          }
+          issues(first: 10, states: OPEN) {
+            edges {
+              node {
+                title
+              }
+            }
+          }
+        }
+      }
+    `,
+    { repo }
+  );
+  return {
+    users: assignableUsers,
+    issues
+  };
+}
+
+async function findOrCreateIssue({ keys, users, issues }) {
+  console.log({ keys, users, issues });
+}
+
 async function run() {
+  const token = core.getInput("github-token");
   const keyringDir = core.getInput("keyring-directory");
-  console.log(keyringDir);
+  const repo = core.getInput("repo");
+  const octokit = github.getOctokit(token);
+  const keys = await getKeys(keyringDir);
+  const { users, issues } = await getUsers(octokit, repo);
+  await findOrCreateIssue(keys);
 }
 
 try {
